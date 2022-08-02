59985fe375daf8603b6d84044aacd961f4101bba
VT1\uix13207 <eleusispc@live.com.mx>
2022-08-02 10:42:32 -0500

    comienzo del proyecto

---
+++ 1)_Testing_Hello_world/Another.py
@@ -0,0 +1,6 @@
+#   ----another----
+
+myDi = {'name':'juan','age':20,'tel':20320102}
+
+keys= myDi.keys()
+print(keys)
---
+++ 1)_Testing_Hello_world/Hello_world.py
@@ -0,0 +1,8 @@
+#t  --- Testing hellow world with python & git
+
+print("Hello worl")
+
+mylist = [1,2,3,4,5,6]
+
+for l in mylist:
+    print(l)
